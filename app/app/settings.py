a08640bd9b5f86f5a65ddbe738a9ce30dd3779bf
jstrieb/github-stats <vadnetvadnet@ukr.net>
2024-09-22 23:45:12 +0300

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -134,11 +134,12 @@ DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:8000",
-    "http://localhost:8001",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:8001",
+    "http://0.0.0.0:8000",
 ]
 
 CORS_ORIGINS_AllOW_ALL = True
